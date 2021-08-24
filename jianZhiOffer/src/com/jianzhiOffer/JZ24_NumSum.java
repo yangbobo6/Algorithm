c4221e5c95b70934e0cf0ccedb7e5490066e4e4c
yyangbobo <1360664142@qq.com>
2021-08-24 10:36:00 +0800

    24 树的路径上面的和等于一个数，求所有路径

--- jianZhiOffer/src/com/jianzhiOffer/JZ24_NumSum.java
+++ jianZhiOffer/src/com/jianzhiOffer/JZ24_NumSum.java
@@ -8,35 +8,41 @@ public class JZ24_NumSum {
         int temp = 0;
         int n = target;
         ArrayList<ArrayList<Integer>> arrayLists = new ArrayList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
         if(root == null){
-            return null;
+            return arrayLists;
         }
-        Traverse(arrayLists,root,temp,n);
+        Traverse(arrayLists,arrayList,root,temp,n);
         //arrayLists.add(SumNumOfTheTreeNode(root,temp,n));
         return arrayLists;
     }
 
     //计算本层 二叉树 值的和  与n作比较
-    public ArrayList<Integer> SumNumOfTheTreeNode(TreeNode treeNode,int temp, int n){
+    public ArrayList<Integer> SumNumOfTheTreeNode(ArrayList<Integer> arrayList,TreeNode treeNode,int temp, int n){
         //treeNode是存在的
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        arrayList1.addAll(arrayList);
         if(n>=temp){
-            arrayList.add(treeNode.val);
-            return arrayList;
+            arrayList1.add(treeNode.val);
+            return arrayList1;
         }else {
+            //销毁 arraylist
             return null;
         }
     }
 
     //遍历二叉树
-    public void Traverse(ArrayList<ArrayList<Integer>> arrayLists,TreeNode treeNode, int temp,int n){
+    public void Traverse(ArrayList<ArrayList<Integer>> arrayLists,ArrayList<Integer> arrayList,TreeNode treeNode, int temp,int n){
         temp = temp + treeNode.val;
-        arrayLists.add(SumNumOfTheTreeNode(treeNode,temp,n));
+        arrayList = SumNumOfTheTreeNode(arrayList,treeNode,temp,n);
+        if(n==temp&&treeNode.left==null&&treeNode.right==null){
+            arrayLists.add(arrayList);
+        }
         if(treeNode.left!=null){
-            Traverse(arrayLists,treeNode.left,temp,n);
+            Traverse(arrayLists,arrayList,treeNode.left,temp,n);
         }
         if(treeNode.right!=null){
-            Traverse(arrayLists,treeNode.right,temp,n);
+            Traverse(arrayLists,arrayList,treeNode.right,temp,n);
         }
     }
 }
