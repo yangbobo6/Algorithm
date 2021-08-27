dae3b902f112ad7aca0e9f5e4a4efbaa8946049d
yyangbobo <1360664142@qq.com>
2021-08-27 23:00:14 +0800

    递归 求和

--- jianZhiOffer/src/com/jianzhiOffer/JZ47_SumOfNumber.java
+++ jianZhiOffer/src/com/jianzhiOffer/JZ47_SumOfNumber.java
@@ -1,5 +1,26 @@
 package com.jianzhiOffer;
 
 public class JZ47_SumOfNumber {
-    
+    int sum = 0;
+    public int Sum_Solution1(int n) {
+        if(n==1){
+            return n;
+        }
+        else{
+            return n+Sum_Solution1(n-1);
+        }
+    }
+
+    public int Sum_Solution(int n){
+        int sum = n;
+        boolean ans = n>0&&(sum+=Sum_Solution(n-1))>0;
+        return sum;
+    }
+
+
+
+    public static void main(String[] args) {
+        JZ47_SumOfNumber jz47_sumOfNumber = new JZ47_SumOfNumber();
+        System.out.println(jz47_sumOfNumber.Sum_Solution1(10));
+    }
 }
