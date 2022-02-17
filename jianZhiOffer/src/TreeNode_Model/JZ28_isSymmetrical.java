6ec2d7ab9fd69f72737b1c29260017ac7885eb95
yyangbobo <1360664142@qq.com>
2022-02-17 22:39:05 +0800

    左右子树是否相同

---
+++ jianZhiOffer/src/TreeNode_Model/JZ28_isSymmetrical.java
@@ -0,0 +1,24 @@
+package TreeNode_Model;
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
+/**
+ * @Author: yangbo
+ * @Date: 2022-02-17-20:45
+ * @Description:
+ */
+public class JZ28_isSymmetrical {
+    boolean isSymmetrical(TreeNode pRoot) {
+        
+
+        return true;
+    }
+}
