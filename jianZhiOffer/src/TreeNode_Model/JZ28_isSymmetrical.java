569a02c99f70f4efa5758d47ce38fc50601c5094
yyangbobo <1360664142@qq.com>
2022-02-18 13:41:10 +0800

    82

--- jianZhiOffer/src/TreeNode_Model/JZ28_isSymmetrical.java
+++ jianZhiOffer/src/TreeNode_Model/JZ28_isSymmetrical.java
@@ -1,5 +1,4 @@
 package TreeNode_Model;
-
 class TreeNode {
     int val = 0;
     TreeNode left = null;
@@ -8,17 +7,31 @@ class TreeNode {
     public TreeNode(int val) {
         this.val = val;
     }
-
 }
+
 /**
  * @Author: yangbo
  * @Date: 2022-02-17-20:45
  * @Description:
  */
+
 public class JZ28_isSymmetrical {
     boolean isSymmetrical(TreeNode pRoot) {
-        
+        return pRoot==null&&isSuccess(pRoot.left,pRoot.right);
+    }
 
-        return true;
+    public boolean isSuccess(TreeNode l,TreeNode r){
+        if(l==null&&r==null){
+            return true;
+        }else if(l==null||r==null){
+            return false;
+        }
+
+        if(l.val!=r.val){
+            return false;
+        }else {
+            return isSuccess(l.left,r.right)&&isSuccess(l.right,r.left);
+        }
+
     }
 }
