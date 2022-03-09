73ef6bce3f855411ca670f727388baed59ca0184
yyangbobo <1360664142@qq.com>
2022-03-09 22:46:40 +0800

    提交

--- LeetCode_Classification/src/tree/code101_isSymmetric.java
+++ LeetCode_Classification/src/tree/code101_isSymmetric.java
@@ -7,7 +7,26 @@ package tree;
  */
 public class code101_isSymmetric {
     public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        boolean b = isInorderSymmetric(root.left,root.right);
+        return b;
+    }
 
-        return true;
+    public boolean isInorderSymmetric(TreeNode left,TreeNode right){
+        //递归的终点  两个节点都为空
+        if(left==null&&right==null){
+            return true;
+        }
+        //两个节点有一个为空   则不对称
+        if(left==null || right ==null ){
+            return false;
+        }
+        //两个节点都存在  但是值不一样  不对称
+        if(left.val!=right.val){
+            return false;
+        }
+        return isInorderSymmetric(left.left,right.right)&&isInorderSymmetric(left.right,right.left);
     }
 }
--- LeetCode_Classification/src/tree/code104_maxDepth.java
+++ LeetCode_Classification/src/tree/code104_maxDepth.java
@@ -3,8 +3,21 @@ package tree;
 /**
  * @Author: yangbo
  * @Date: 2022-03-08-23:35
- * @Description:
+ * @Description:   求二叉树最大深度
  */
 public class code104_maxDepth {
-    
+    public int maxDepth(TreeNode root) {
+        int depth = inorderDepth(root);
+        return depth;
+    }
+
+    public int inorderDepth(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left = inorderDepth(root.left);
+        int right = inorderDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+
 }
