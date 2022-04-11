b851e968fafab8b5e8f497b4d846732338add408
yyangbobo <1360664142@qq.com>
2022-04-11 12:09:52 +0800

    递归

--- LeetCode_Classification/src/dp/code70_climbStairs.java
+++ LeetCode_Classification/src/dp/code70_climbStairs.java
@@ -1,12 +1,31 @@
 package dp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: yangbo
  * @Date: 2022-04-09-22:19
- * @Description:
+ * @Description: 上台阶问题
  */
 public class code70_climbStairs {
     public int climbStairs(int n) {
-        return n;
+        int[] record = new int[n];
+        int res = climbStairs(n,record);
+        
+        return res;
+    }
+    
+    public int climbStairs(int n,int[] record){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(record[n]==0){
+            record[n] = climbStairs(n-1,record)+climbStairs(n-2,record); 
+        }
+        return record[n];
     }
 }
