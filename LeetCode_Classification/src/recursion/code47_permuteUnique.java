f19378025a484b72457ba080dde6e70e3c035da0
yyangbobo <1360664142@qq.com>
2022-04-27 16:24:02 +0800

    leetcode 47recursion增加剪枝

--- LeetCode_Classification/src/recursion/code47_permuteUnique.java
+++ LeetCode_Classification/src/recursion/code47_permuteUnique.java
@@ -1,6 +1,7 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /**
  * @Author: yangbo
@@ -11,27 +12,33 @@ public class code47_permuteUnique {
     public List<List<Integer>> permuteUnique(int[] nums) {
         int len = nums.length;
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        
         boolean[] used = new boolean[len];
-        int depth = 0;
         List<Integer> ans = new ArrayList<>();
-        dfs(nums,depth,len,res,used,ans);
+        dfs(nums,0,len,res,used,ans);
         return res;
     }
     public void dfs(int[] nums,int depth,int len,List<List<Integer>> res,boolean[] used,List<Integer> ans){
         if(depth == len){
-            List<Integer> path = new ArrayList<>(ans);
-            if (!res.contains(path)){
-                res.add(path);
-            }else {
-                return;
-            }
-            
+//            List<Integer> path = new ArrayList<>(ans);
+//            if (!res.contains(path)){
+//                res.add(path);
+//            }else {
+//                return;
+//            }
+            res.add(new ArrayList<>(ans));
+            return;
         }
+        
         for(int i = 0;i<len;i++){
             if(!used[i]){
+                if(i>0 && nums[i]==nums[i-1] && !used[i-1]){
+                    continue;
+                }
                 ans.add(nums[i]);
                 used[i] = true;
-                dfs(nums,depth,len,res,used,ans);
+                dfs(nums,depth+1,len,res,used,ans);
                 used[i] = false;
                 ans.remove(ans.size()-1);
             }
