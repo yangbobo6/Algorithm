9b5e9164c4f22d823344e1a45dadebcc81c27fc4
yyangbobo <1360664142@qq.com>
2022-06-23 21:50:10 +0800

    use deque in project

--- LeetCode_Classification/src/dp/code337_rob3.java
+++ LeetCode_Classification/src/dp/code337_rob3.java
@@ -48,5 +48,36 @@ public class code337_rob3 {
         map.put(root,result);
         return result;
     }
+
+    
+    
+    //动态规划
+    private int rob3(TreeNode root){
+        int[] result = robDP(root);
+        return Math.max(result[0],result[1]);
+    }
+    private int[] robDP(TreeNode root){
+        if(root==null){
+            return new int[2];
+        }
+        int result[] = new int[2];
+        int[] left = robDP(root.left);
+        int[] right = robDP(root.right);
+        
+        //0代表不偷  1代表偷
+        //当前节点选择不偷：当前节点能偷到的最大钱数 = 左孩子能偷到的钱 + 右孩子能偷到的钱
+        result[0] = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        //当前节点选择偷：当前节点能偷到的最大钱数 = 左孩子选择自己不偷时能得到的钱 + 右孩子选择不偷时能得到的钱 + 当前节点的钱数
+        result[1] = left[0]+right[0]+root.val;
+        return result;
+    }
+
+   /* public static void main(String[] args) {
+        int[] preOrder = new int[]{6,3,1,4,2};
+        int[] inOrder = new int[]{1,3,4,6,2};
+        
+        TreeNode root = buildTree(preOrder,inOrder);
+        rob3(root);
+    }*/
     
 }
